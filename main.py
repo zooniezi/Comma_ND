38177675e7ca90d0796ab35c389467774705b451
Jaejun Kim <nicy130402@naver.com>
2022-08-02 20:44:17 +0900

    Day2 finished
    깔때기 완성 및 공 1000개 생성 완료!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame as pg
 import pymunk
 import pymunk.pygame_util
 import math
+import random
 
 pg.init()
 
@@ -11,7 +12,7 @@ HEIGHT = 800
 
 k = 10
 b = 100
-c = 15
+c = 17
 d = 30
 
 L1, L2, L3, L4 = (k, -100), (k, b), (WIDTH/2 - c, 200), (WIDTH/2 - c , 200+d)
@@ -23,9 +24,21 @@ rightLine = [R1, R2, R3, R4]
 window = pg.display.set_mode((WIDTH, HEIGHT))
 
 def draw(space, window, drawoption):
-    window.fill("white")
+    window.fill("black")
     space.debug_draw(drawoption)
     pg.display.update()
+
+def random_color(): #클릭을 할때마다 공의 색깔을 결정하기 위해서
+    red=random.randint(0,255)
+    green=random.randint(0,255)
+    blue=random.randint(0,255)
+    opa=random.randint(0,100)
+    return (red,green,blue,opa) #이렇게 red,green,blue,opa를 random으로 뽑아내고 밑에 create_ball에서 얘네가 클릭될 때마다 새롭게 찍히고 있는것
+
+def random_postion(): #공의 position을 랜덤으로 정하기
+    x=random.randint(50,950)
+    y=random.randint(-100,100)
+    return(x,y)
 
 def createBall(space, position):
     radius = 7
@@ -33,9 +46,9 @@ def createBall(space, position):
     ballBody.position = position
     ballShape = pymunk.Circle(ballBody, radius)
     ballShape.mass = 100
-    ballShape.color = (255,0,0,100)
-    ballShape.elasticity = 0.9
-    ballShape.friction = 0.4
+    ballShape.color = random_color()
+    ballShape.elasticity = 0.2
+    ballShape.friction = 0.2
     space.add(ballBody, ballShape)
 
     return ballShape
@@ -71,9 +84,10 @@ def run(window, width, height):
     fps = 60
     deltaTime = 1/fps
     realSpace = pymunk.Space()
-    realSpace.gravity = (0, 981)
+    realSpace.gravity = (0, 3500)
 
-    realBall = createBall(realSpace, (WIDTH/2, HEIGHT/2))
+    for i in range(1000):
+        realBall=createBall(realSpace,random_postion()) #realSpace에 공 만들기    
     createWall(realSpace)
     makeFunnel(realSpace)
     drawOptions = pymunk.pygame_util.DrawOptions(window)
@@ -84,9 +98,9 @@ def run(window, width, height):
             if nowEvent.type == pg.QUIT:
                 run = False
                 break
-            if nowEvent.type == pg.MOUSEBUTTONDOWN:
-                if nowEvent.button == 1:
-                    createBall(realSpace, pg.mouse.get_pos())
+            # if nowEvent.type == pg.MOUSEBUTTONDOWN:
+            #     if nowEvent.button == 1:
+            #         createBall(realSpace, pg.mouse.get_pos())
         draw(realSpace, window, drawOptions)
         realSpace.step(deltaTime)
         clock.tick(fps)
